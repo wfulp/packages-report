45064f977e086e13e37ee28cb739269d16e964c5
Jimmy Fulp <williamjfulp@gmail.com>
2018-02-01 13:31:06 -0800

    Update 3rd script

--- R/03_barchart-packages-built.R
+++ R/03_barchart-packages-built.R
@@ -1,6 +1,8 @@
 ## make a barchart from the frequency table in data/add-on-packages-freqtable.csv
-
 ## read that csv into a data frame, then ...
+library(data.table)
+library(ggplot2)
+apt_freqtable <- fread('../data/add-on-packages-freqtable.csv')
 
 ## if you use ggplot2, code like this will work:
 ggplot(apt_freqtable, aes(x = Built, y = n)) +
@@ -8,7 +10,7 @@ ggplot(apt_freqtable, aes(x = Built, y = n)) +
 
 ## write this barchart to figs/built-barchart.png
 ## if you use ggplot2, ggsave() will help
-
+ggsave('../figs/built-barchart.png')
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 
